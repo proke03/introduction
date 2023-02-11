393b43ca7eb8972294599d97d994f7328ea28175
JM <proke03@naver.com>
2023-02-11 20:45:57 +0900

    design: add first section layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '@/styles/Home.module.css';
+import styled from '@emotion/styled';
+import image_profile from '@/public/profile.jpg'
 
 export default function Home() {
   return (
@@ -12,109 +14,113 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <StyledWrap>
+          <header></header>
+          <section>
+            <div className="section__content">
+              <div className="section__container">
+                <div className="section__container-name">
+                  <div className="section__container-name-bar" />
+                  <h2>Min Jang</h2>
+                  <div className="section__container-name-bar" />
+                </div>
+                <div className="section__container-info">
+                  <h4>FRONT-END DEVELOPER</h4>
+                  <hr />
+                  <p>Email: jangmin.dev@gmail.com<br />Github: https://github.com/proke03</p>
+                  {/* <p>Github: https://github.com/proke03</p> */}
+                </div>
+              </div>
+              <div id="profile_image" />
+            </div>
+          </section>
+        </StyledWrap>
       </main>
     </>
   )
 }
+
+const StyledWrap = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+
+  header{
+    height: 72px;
+  }
+
+  section{
+    height: 785px;
+    display: flex;
+    flex-direction: row;
+    padding: 0 auto;
+
+    div.section__content{
+      margin: 0 auto;
+      width: 1140px;
+      height: 100%;
+      display: grid;
+      grid-template-columns: 50% 50%;
+
+      div.section__container{
+        div.section__container-name{
+          padding: 30px 40px 30px 0;
+          /* font-family: Roboto; */
+
+          div.section__container-name-bar{
+            display: block;
+            width: 456px;
+            height: 20px;
+            background-color: #f1c50e;
+          }
+
+          h2{
+            margin: 18px 0;
+            font-size: 3.75rem;
+          }
+        }
+
+        .section__container-name-bar:nth-child(1){
+          margin-top: 150px;
+        }
+
+        .section__container-name-bar:nth-child(3){
+          margin-bottom: 201px;
+        }
+
+        div.section__container-info{
+          padding: 30px 40px 30px 0;
+
+          h4{
+            margin: 18px 0 0;
+            font-size: 1.25rem;
+            letter-spacing: 2px;
+            font-weight: 700;
+          }
+
+          hr{
+            margin: 23px auto 0 0;
+            width: 489px;
+            border: 1px solid black;
+          }
+
+          p{
+            margin: 20px 0 0;
+            font-size: 1rem;
+            line-height: 1.6;
+          }
+        }
+      }
+
+      div#profile_image{
+        min-height: 785px;
+        background-image: url("/profile.jpg");
+        background-size: cover;
+        background-position: 50% 50%;
+      }
+    }
+  }
+`;
