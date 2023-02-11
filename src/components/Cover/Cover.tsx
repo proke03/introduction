cb93f9ce224e9bbcc20d1fd2e86087a5485e74c2
JM <proke03@naver.com>
2023-02-11 21:03:06 +0900

    refactor: extract Cover.tsx from index.tsx

---
+++ src/components/Cover/Cover.tsx
@@ -0,0 +1,111 @@
+import styled from '@emotion/styled';
+
+export default function Cover() {
+  return(
+    <StyledWrap>
+      <header></header>
+      <section>
+        <div className="section__content">
+          <div className="section__container">
+            <div className="section__container-name">
+              <div className="section__container-name-bar" />
+              <h2>Min Jang</h2>
+              <div className="section__container-name-bar" />
+            </div>
+            <div className="section__container-info">
+              <h4>FRONT-END DEVELOPER</h4>
+              <hr />
+              <p>Email: jangmin.dev@gmail.com<br />Github: https://github.com/proke03</p>
+            </div>
+          </div>
+          <div id="profile_image" />
+        </div>
+      </section>
+    </StyledWrap>
+  )
+}
+
+const StyledWrap = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+
+  header{
+    height: 72px;
+  }
+
+  section{
+    height: 785px;
+    display: flex;
+    flex-direction: row;
+    padding: 0 auto;
+
+    div.section__content{
+      margin: 0 auto;
+      width: 1140px;
+      height: 100%;
+      display: grid;
+      grid-template-columns: 50% 50%;
+
+      div.section__container{
+        div.section__container-name{
+          padding: 30px 40px 30px 0;
+          /* font-family: Roboto; */
+
+          div.section__container-name-bar{
+            display: block;
+            width: 456px;
+            height: 20px;
+            background-color: #f1c50e;
+          }
+
+          h2{
+            margin: 18px 0;
+            font-size: 3.75rem;
+          }
+        }
+
+        .section__container-name-bar:nth-child(1){
+          margin-top: 150px;
+        }
+
+        .section__container-name-bar:nth-child(3){
+          margin-bottom: 201px;
+        }
+
+        div.section__container-info{
+          padding: 30px 40px 30px 0;
+
+          h4{
+            margin: 18px 0 0;
+            font-size: 1.25rem;
+            letter-spacing: 2px;
+            font-weight: 700;
+          }
+
+          hr{
+            margin: 23px auto 0 0;
+            width: 489px;
+            border: 1px solid black;
+          }
+
+          p{
+            margin: 20px 0 0;
+            font-size: 1rem;
+            line-height: 1.6;
+          }
+        }
+      }
+
+      div#profile_image{
+        min-height: 785px;
+        background-image: url("/profile.jpg");
+        background-size: cover;
+        background-position: 50% 50%;
+      }
+    }
+  }
+`;
