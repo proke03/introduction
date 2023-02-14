08c4c29938df1924c815e8e531c5275c527fd2f3
JM <proke03@naver.com>
2023-02-14 18:52:26 +0900

    fix: build error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const AnyComponent = Component as any;
+  return <AnyComponent {...pageProps} />
 }
