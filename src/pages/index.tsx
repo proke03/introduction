93ca7974dd38b55ec9c05a59a86b7be76c596411
JM <proke03@naver.com>
2023-02-25 14:17:55 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <meta name="author" content="장민" />
         <meta property="og:title" content="프론트엔드 개발자 장민" />
         <meta property="og:description" content="안녕하세요, 프론트엔드 개발자 장민입니다." />
-        <meta property="og:image" content="https://user-images.githubusercontent.com/19148682/221339578-7beeeffd-0392-4eb6-be28-9e8798b43b67.jpg" />
+        <meta property="og:image" content="https://user-images.githubusercontent.com/19148682/221339578-7beeeffd-0392-4eb6-be28-9e8798b43b67.jpg?raw=1" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
